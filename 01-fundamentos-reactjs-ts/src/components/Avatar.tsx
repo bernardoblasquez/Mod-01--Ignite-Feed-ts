aecd37458833b4c92f8ed81b747d59334b3d1721
Bernardo <bernardo.bt@gmail.com>
2022-10-18 06:49:16 -0300

    feat: uso de ImgHTMLAttributes, rest e spread operator. Facilitar passagem de atributos

--- 01-fundamentos-reactjs-ts/src/components/Avatar.tsx
+++ 01-fundamentos-reactjs-ts/src/components/Avatar.tsx
@@ -1,10 +1,30 @@
+import { ImgHTMLAttributes } from 'react';
 import styles from './Avatar.module.css';
 
-export function Avatar({hasBorder = true, src}) {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement>{
+   hasBorder?: boolean;
+}
+
+export function Avatar({hasBorder = true, ...props}: AvatarProps) {
+
+   console.log(props)
    return(
       <img 
          className={hasBorder ? styles.avatarWithBorder : styles.avatar} 
-         src={src}
-         alt="" />
+         {...props}
+      />
    )
 }
+
+/*
+   Extender AvatarProps usando ImgHTMLAttributes é forma mais facilitada de 
+   escrever uma interface para IMG, sem a necessidade de declarar cada um dos
+   parâmetros da imagem. Nesse caso é necessário adicionar somente os atributos
+   que não existem em ImgHTMLAttributes
+
+   ..props = nesse contexto (atributos da função) é chamado de rest operator, já
+   que pega todos os outros atributos que nõa foram declarados
+
+   {...props} = spread operator, pega cada valor dentro de props e passa para  
+
+*/
